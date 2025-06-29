2ec42e1ad133c8e5ac9a2c8acfd629b587a0510f
anamta-ansari <161108965+anamta-ansari@users.noreply.github.com>
2025-06-29 13:40:26 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { FaFacebook, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import SkillsComponent from "./Components/Skills"
 import ProjectComponent from "./Components/Project"
